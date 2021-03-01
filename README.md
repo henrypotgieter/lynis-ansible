08e2736371d2877504808a40d69dd066ba2598ff
henry.potgieter <henry.potgieter@gtt.net>
2021-03-01 16:22:08 -0500

    Minor changes

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ the audit process locally on each system.  Gather reports back to master system 
 # Usage
 Update your /etc/ansible/hosts and define activehosts (or whatever group of
 hosts you like) to conduct the audit service against.
+
+Run the python script to automatically download the latest version of the lynis
+software package.  Place this file into wherever you store your ansible static
+files (/ansible/static_files typically)
+
+Execute the playbook and wait for it to process through each of the hosts.
+
+Finally check your /tmp directory, you should see all the reports contained
+under /tmp/lynis-report/
--- update.py
+++ update.py
@@ -9,22 +9,23 @@ Script to scrape the current latest version of lynis from cisofy.com, download
 the file to disk and then verify the checksum
 """
 
+destination = "/ansible/static_files/lynis-latest.tar.gz"
+
 # Scrape downloads page and get the file
 response = requests.get('https://cisofy.com/downloads/lynis/')
 contents = str(response.content)
 page_contents = contents.split('.tar.gz" class="button-green button-small inline left"')
 filename = page_contents[0].split('https://downloads.cisofy.com/lynis/')
 download_file = "https://downloads.cisofy.com/lynis/" + filename[1] + ".tar.gz"
-urllib.request.urlretrieve(download_file, "lynis-latest.tar.gz")
-print("Downloaded " + filename[1] + " version, written to lynis-latest.tar.gz")
+urllib.request.urlretrieve(download_file, destination)
+print("Downloaded " + filename[1] + " version, written to " + destination)
 
 # Extract the sha256 hash from the website
 sha_sum_begin = page_contents[1].split('SHA256 hash: ')
 
 # Get the sha256 hash of the file
-file_to_hash = "lynis-latest.tar.gz"
 sha256_hash = hashlib.sha256()
-with open(file_to_hash,"rb") as f:
+with open(destination,"rb") as f:
     # Read and update hash string value in blocks of 4K
     for byte_block in iter(lambda: f.read(4096),b""):
         sha256_hash.update(byte_block)
